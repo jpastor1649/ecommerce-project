a9c1fc9fd60005c13541423a17a2d931b6da50d3
saospinav <saospinav@unal.edu.co>
2026-03-18 02:28:22 -0500

    style: format main.py with black

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI(
-    title="Ecommerce B2C Colombia",
-    version="0.1.0"
-)
+app = FastAPI(title="Ecommerce B2C Colombia", version="0.1.0")
+
 
 @app.get("/")
 def root():
     return {"status": "ok", "message": "API funcionando"}
+
 
 @app.get("/health")
 def health():
