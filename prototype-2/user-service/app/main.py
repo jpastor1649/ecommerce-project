5083a1b762d8d997639d8a980c97b0b929b18d40
jpastor1649 <jpastor@unal.edu.co>
2026-04-18 00:16:18 -0500

    chore(cleanup): remove legacy backend and prototype files

--- prototype-2/user-service/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from .db import Base, engine
-from .routes import router
-
-app = FastAPI()
-
-# Table creation
-Base.metadata.create_all(bind=engine)
-app.include_router(router)
